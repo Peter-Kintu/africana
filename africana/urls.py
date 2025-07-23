60a1aee265b21059021dbbf7673584c19523f7d8
Kintu Peter <kintupeter721@gmail.com>
2025-07-23 22:35:32 +0300

    first commit

--- africana/urls.py
+++ africana/urls.py
@@ -1,20 +1,22 @@
-# learnflow_ai/django_backend/django_backend/urls.py
+# learnflow_ai/django_backend/africana/urls.py
 
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from rest_framework.authtoken import views as drf_authtoken_views
+from rest_framework.authtoken.views import obtain_auth_token
+from django.conf import settings # Import settings
+from django.conf.urls.static import static # Import static
+from api.views import teacher_dashboard_view # Import the view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Include your API app's URLs
-    path('api-token-auth/', drf_authtoken_views.obtain_auth_token, name='api_token_auth'),
-    # This line MUST be present for /accounts/login/ to work
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('accounts/', include('django.contrib.auth.urls')), # For Django's auth views
+    path('teacher-dashboard/', teacher_dashboard_view, name='teacher-dashboard'), # ADDED: Teacher Dashboard URL
 ]
 
-# Serve media files during development
+# Serve static and media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
