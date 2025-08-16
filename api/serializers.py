8a73b72a0437321ac37346fbf14899b373be32a9
Kintu Peter <kintupeter721@gmail.com>
2025-08-16 22:56:52 +0300

    _r

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,7 @@
 
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Student, Lesson, Question, QuizAttempt, StudentProgress, GENDER_CHOICES, QUESTION_TYPE_CHOICES, DIFFICULTY_CHOICES
+from .models import Student, Teacher, Lesson, Question, QuizAttempt, StudentProgress
 import json
 
 class UserSerializer(serializers.ModelSerializer):
@@ -25,100 +25,7 @@ class UserSerializer(serializers.ModelSerializer):
         )
         return user
 
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Student
-        fields = '__all__'
-
-    def create(self, validated_data):
-        user_id = validated_data.pop('user_id', None)
-        if user_id:
-            try:
-                user = User.objects.get(id=user_id)
-                validated_data['user'] = user
-            except User.DoesNotExist:
-                raise serializers.ValidationError({"user_id": "User with this ID does not exist."})
-        return super().create(validated_data)
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = '__all__'
-
-class QuestionSerializer(serializers.ModelSerializer):
-    lesson_uuid = serializers.PrimaryKeyRelatedField(
-        queryset=Lesson.objects.all(),
-        source='lesson',
-        write_only=True
-    )
-    
-    class Meta:
-        model = Question
-        fields = '__all__'
-        read_only_fields = ('lesson',)
-
-class QuizAttemptSerializer(serializers.ModelSerializer):
-    student_user_id = serializers.IntegerField(write_only=True)
-    question_uuid = serializers.PrimaryKeyRelatedField(
-        queryset=Question.objects.all(),
-        source='question',
-        write_only=True
-    )
-
-    class Meta:
-        model = QuizAttempt
-        fields = '__all__'
-        read_only_fields = ('student', 'question')
-    
-    def create(self, validated_data):
-        student_user_id = validated_data.pop('student_user_id', None)
-        question_uuid = validated_data.pop('question', None)
-
-        if not student_user_id:
-            raise serializers.ValidationError({"student_user_id": "This field is required."})
-
-        if not question_uuid:
-            raise serializers.ValidationError({"question_uuid": "This field is required."})
-
-        try:
-            student = Student.objects.get(user__id=student_user_id)
-        except Student.DoesNotExist:
-            raise serializers.ValidationError({"student_user_id": "Student with this user ID does not exist."})
-
-        try:
-            question = Question.objects.get(uuid=question_uuid.uuid)
-        except Question.DoesNotExist:
-            raise serializers.ValidationError({"question_uuid": "Question with this UUID does not exist."})
-
-        validated_data['student'] = student
-        validated_data['question'] = question
-
-        return super().create(validated_data)
-
-class StudentProgressSerializer(serializers.ModelSerializer):
-    student_user_id = serializers.IntegerField(write_only=True)
-
-    class Meta:
-        model = StudentProgress
-        fields = '__all__'
-        read_only_fields = ('student',)
-
-    def create(self, validated_data):
-        student_user_id = validated_data.pop('student_user_id', None)
-        if student_user_id:
-            try:
-                student = Student.objects.get(user__id=student_user_id)
-                validated_data['student'] = student
-            except Student.DoesNotExist:
-                raise serializers.ValidationError({"student_user_id": "Student with this user ID does not exist."})
-        return super().create(validated_data)
-
-    def update(self, instance, validated_data):
-        overall_progress_data = validated_data.get('overall_progress_data')
-        if overall_progress_data and isinstance(overall_progress_data, str):
-            try:
-                validated_data['overall_progress_data'] = json.loads(overall_progress_data)
-            except json.JSONDecodeError:
-                raise serializers.ValidationError({"overall_progress_data": "Invalid JSON format for overall_progress_data."})
-        
-        return super().update(instance, validated_data)
+class AuthSerializer(serializers.ModelSerializer):
+    is_teacher = serializers.SerializerMethodField()
+    is_student = serializers.SerializerMethodField()
+    teacher_uuid = serializers.Serializer
