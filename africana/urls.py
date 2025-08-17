f22ddcd567950b770896af7462f746c5b2eb3fc4
Kintu Peter <kintupeter721@gmail.com>
2025-08-17 11:05:10 +0300

    _r

--- africana/urls.py
+++ africana/urls.py
@@ -2,13 +2,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken import views
 from api.views import home 
+from rest_framework.authtoken import views
 
 urlpatterns = [
+    path('', home, name='home'),  # This must be the first line
     path('admin/', admin.site.urls),
+    path('api/v1/', include('api.urls')),
     path('api-token-auth/', views.obtain_auth_token),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', home, name='home'),
-    path('api/', include('api.urls')),
 ]
