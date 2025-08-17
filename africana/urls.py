c5938f42f3c57858c039d3ae60478572b8dab529
Kintu Peter <kintupeter721@gmail.com>
2025-08-17 10:24:06 +0300

    _r

--- africana/urls.py
+++ africana/urls.py
@@ -2,14 +2,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from api.views import home  # Make sure this import matches your file structure
 from rest_framework.authtoken import views
+from api.views import home 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),
     path('api-token-auth/', views.obtain_auth_token),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('teacher-dashboard/', include('api.urls')),
-    path('', home, name='home'),  # This line will connect the root URL to your home view
+    path('', home, name='home'),
+    path('api/', include('api.urls')),
 ]
